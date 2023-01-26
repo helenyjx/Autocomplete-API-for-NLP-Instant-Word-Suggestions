45e51e4834b9cbdc3698eca47f627d57102bdf10
Jiaxin Ying <112274822+helenyjx@users.noreply.github.com>
2023-01-26 01:28:17 +0000

    new

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-// This is a function for a Macro Polo game
-pub fn marco_polo(name: &str) -> String {
-    if name == "Marco" {
-        "Polo".to_string()
-    } else {
-        "Marco".to_string()
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,77 @@
-//A command-line tool that plays Marco Polo
-use clap::Parser;
+use multimap::MultiMap;
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::Path;
+use std::str;
+use warp::{http, Filter};
 
-#[derive(Parser)]
-#[clap(version = "1.0", author = "Jiaxin Ying", about = "A Marco Polo game.")]
-struct Cli {
-    #[clap(subcommand)]
-    command: Option<Commands>,
+#[macro_use]
+extern crate lazy_static;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct Query {
+    term: String,
 }
-
-#[derive(Parser)]
-enum Commands {
-    #[clap(version = "1.0", author = "Jiaxin Ying", about = "A Marco Polo game.")]
-    Marco {
-        #[clap(short, long)]
-        name: String,
-    },
-}
-
-// This is the main function
-// hello::marco_polo(&name)
-fn main() {
-    let args = Cli::parse();
-    match args.command {
-        Some(Commands::Marco { name }) => {
-            println!("{}", hello::marco_polo(&name));
+fn load_suggestions() -> MultiMap<String, String> {
+    let path = Path::new("resources/weighted_strings.txt");
+    let mut autocomplete: MultiMap<String, String> = MultiMap::new();
+    let contens: String = fs::read_to_string(&path).unwrap();
+    for line in contens.lines() {
+        let line_splitted: Vec<&str> = line.split('\t').collect();
+        let string = line_splitted[0];
+        let chars: Vec<char> = string.chars().collect();
+        for idx in 1..chars.len() {
+            let slice: String = chars[0..idx].iter().collect();
+            autocomplete.insert(slice.to_string(), string.to_string());
         }
-        None => println!("No command was used"),
     }
+    autocomplete
+}
+lazy_static! {
+    static ref SUGGESTIONS: MultiMap<String, String> = load_suggestions();
+}
+
+async fn get_suggestions(query: Query) -> Result<impl warp::Reply, warp::Rejection> {
+    let suggested_terms = get_suggestion(&query.term);
+    let top_n_suggestions = take_slice(suggested_terms, 0, 10);
+    Ok(warp::reply::with_status(
+        format!("{:?}", top_n_suggestions),
+        http::StatusCode::OK,
+    ))
+}
+
+fn post_json() -> impl Filter<Extract = (Query,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+fn get_suggestion(query: &str) -> Vec<String> {
+    let suggestion = SUGGESTIONS.get_vec(query);
+    match suggestion {
+        Some(values) => values.to_vec(),
+        None => Vec::<String>::new(),
+    }
+}
+
+fn take_slice(vec: Vec<String>, from_index: usize, to_index: usize) -> Vec<String> {
+    if vec.get(from_index).is_none() || vec.get(to_index).is_none() {
+        Vec::<String>::new()
+    } else {
+        vec[from_index..to_index].to_vec()
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let suggestions_endpoint = warp::post()
+        .and(warp::path("autocomplete"))
+        .and(warp::path("v1"))
+        .and(warp::path::end())
+        .and(post_json())
+        .and_then(get_suggestions);
+
+    let routes = suggestions_endpoint;
+
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
